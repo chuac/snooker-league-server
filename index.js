fb6427aff4f2fe89a44f9c3fc63cd7a007437e8b
Christopher Chua <chuachristopher@hotmail.com>
2020-09-15 20:25:05 +0800

    Don't forget to export the router

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(cors()); // enable cross-origin resource sharing
 
 app.use(express.json()); // parse JSON bodies into JS objects. Is this doubling up? because of body parser? Mosh used this
 // Chris: looks like bodyParser has been built-in to Express as of version 4.16+, so no need for that extra package :thumbsup:
-// app.use('/api/seasons', require('./routes/api/seasons'));
-// app.use('/api/teams', require('./routes/api/teams'));
+app.use('/api/seasons', require('./routes/api/seasons'));
+app.use('/api/teams', require('./routes/api/teams'));
 app.use('/api/players', require('./routes/api/players'));
 
 app.listen(process.env.EXPRESS_PORT || 3000, () => {
