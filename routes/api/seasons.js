6f50f8164a81a4c8d0c5242868fe630df583d2f0
Christopher Chua <chuachristopher@hotmail.com>
2020-09-16 12:36:27 +0800

    Endpoint for league table by specific year

--- routes/api/seasons.js
+++ routes/api/seasons.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/table', async (req, res) => {
-  // return league table for that year
+const { getLadderForSeason } = require("../db");
+
+router.get('/:season/table', async (req, res) => {
+    // return UNSORTED league table for that year, currently an array of team objects with their for/against key/value pairs
+    const season = req.params.season;
+    try {
+        const rows = await getLadderForSeason(season);
+        res.send(rows);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Something went wrong", error });
+    }
 });
 
 router.get('/players', async (req, res) => {
-  // return player statistics for that year
+    // return player statistics for that year
 });
 
 module.exports = router;
--- routes/db.js
+++ routes/db.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 });
 
 module.exports = {
-    pool: pool, // to still be able to run unique queries in other files, if needed
+    pool: pool, // export pool to still be able to run unique queries in other files, if needed
     getAllPlayers: async () => {
         try {
             const query = `SELECT * FROM players;`
@@ -34,6 +34,29 @@ module.exports = {
             return rows;
         } catch (error) {
             console.log(error);
+            return error;
+        }
+    },
+    getLadderForSeason: async (season) => { // the query still contains lots of SQL comments, leaving them in there for now
+        try {
+            const query = `
+                SELECT
+                    team_name,
+                    COALESCE(SUM(home.home_team_score), 0) + COALESCE(SUM(away.away_team_score), 0) AS "for", -- COALESCE runs the first argument if it's not null, otherwise it uses the second argument (0)
+                    COALESCE(SUM(home.away_team_score), 0) + COALESCE(SUM(away.home_team_score), 0) AS "against" --  double quotes for these column aliases because "for" may be a SQL keyword
+                FROM teams
+                LEFT JOIN matches AS home -- left join as that particular team may not always be the home team in that match but we still want to count their score
+                    ON home.home_team_id = teams.team_id
+                LEFT JOIN matches AS away -- as stated above, if that team isn't the home team, they'd have null values so now we're looking for when they were the away team to get their data
+                    ON away.away_team_id = teams.team_id
+                GROUP BY teams.team_id
+                HAVING teams.season = $1 -- need to use HAVING instead of WHERE, if we have a GROUP BY in the query
+                ;`;
+            const values = [season];
+
+            const { rows } = await pool.query(query, values);
+            return rows;
+        } catch (error) {
             return error;
         }
     }
